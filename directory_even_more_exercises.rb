0569a224dc00ea43f7792b8c85f9ce49e3c3af84
ian <kickthechair@gmail.com>
2021-07-21 16:40:44 +0000

    Added functionality to tell the user what options they selected during the menu. Added functionality to load or save custom file names of students. Refactored code further.

--- directory_even_more_exercises.rb
+++ directory_even_more_exercises.rb
@@ -11,25 +11,46 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list of students to the file students.csv"
-  puts "4. Load the list of students from the file students.csv"
+  puts "3. Save the list of students to a .csv file"
+  puts "4. Load the list of students from a .csv file"
   puts "9. Exit program"
 end
 
 def process(selection)
   case selection
     when "1"
+      puts "\nYou entered 1: Input the students"
       input_students
     when "2"
+      puts "\nYou entered 2: Show the students\n\n"
       show_students
     when "3"
-      save_students
+      while true do
+        puts "\nYou entered 3: Save the list of students to a .csv file."
+        puts "What is the name of the file you would like to save students to?"
+        puts "Please ensure you save it with a .csv file extension"
+        save_filename = STDIN.gets.chomp
+        if save_filename.include? ".csv"
+          save_students(save_filename)
+          break
+        end
+      end
     when "4"
-      load_students
+      while true do
+        puts "\nYou entered 4: Load the list of students from a .csv file"
+        puts "Which .csv file would you like to load from?"
+        
+        load_filename = STDIN.gets.chomp
+        if load_filename.include? ".csv"
+          load_students(load_filename)
+          break
+        end
+      end
     when "9"
+      puts "\nYou entered 9: Exit program"
       exit
     else
-      puts "I don't understand your command, please try again"
+      puts "\nI don't understand your command, please try again"
   end
 end
 
@@ -40,52 +61,23 @@ def show_students
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return (enter) twice"
-
+  puts "\nPlease enter the names of the students. To finish, just hit return (enter) twice"
   # Get the first name
   name = STDIN.gets.chomp
   # if name is an empty string - repeat this code
   while !name.empty? do
-    puts "Please enter their hobby"
-    hobby = STDIN.gets.chomp
-    if hobby == ""
-      hobby = "not entered"
-    end
-    
-    puts "Please enter their current occupation"
-    occupation = STDIN.gets.chomp
-    if occupation == ""
-      occupation = "not entered"
-    end
-    
-    puts "Please enter their nationality"
-    nationality = STDIN.gets.chomp
-    if nationality == ""
-      nationality = "not entered"
-    end
-    
-    puts "Please enter their cohort month"
-    cohort = STDIN.gets.chomp
-
-    while cohort.empty?
-      puts "You didn't include their cohort - Please enter one now."
-      cohort = STDIN.gets.chomp
-      if !cohort.empty?
-        break
-      end
-    end
-    
-    # Add the student hash to the array
+    # Call methods to add student information
+    hobby = add_hobby
+    occupation = add_occupation
+    nationality = add_nationality
+    cohort = add_cohort
+    # Add the student information hash to the student array
     add_students_to_array(name, cohort, hobby, occupation, nationality)
-    
-    
     if @students.length == 1
-      puts "Now we have #{@students.length} student"
+      puts "\nNow we have #{@students.length} student"
     else
-      puts "Now we have #{@students.length} students"
+      puts "\nNow we have #{@students.length} students"
     end
-    
     # Get another name from the user
     puts "Please enter another students name or press return to finish adding students."
     name = STDIN.gets.chomp
@@ -99,7 +91,6 @@ end
 
 def print_students_list
   existing_cohorts = @students.map { |student| student[:cohort] }
-
   existing_cohorts.uniq.each do |month|
     puts "#{month} cohort students:".upcase
     @students.each do |student|
@@ -122,9 +113,9 @@ def print_footer
   end
 end
 
-def save_students
+def save_students(save_filename)
   #open file for writing
-  file = File.open("students.csv", "w")
+  file = File.open(save_filename, "w")
   #iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort], student[:hobby], student[:occupation], student[:nationality]]
@@ -134,9 +125,9 @@ def save_students
   file.close
 end
 
-def load_students(filename = "students.csv")
+def load_students(load_filename = "students.csv")
   # open file for loading
-  file = File.open(filename, "r")
+  file = File.open(load_filename, "r")
   # iterate over each line in students.csv
   file.readlines.each do |line|
     # Parallel assignment. Take each line in students.csv, remove trailing new line,
@@ -163,6 +154,46 @@ end
 
 def add_students_to_array(name, cohort, hobby, occupation, nationality)
   @students << {name: name.capitalize, cohort: cohort, hobby: hobby, occupation: occupation, nationality: nationality.capitalize}
+end
+
+def add_hobby
+  puts "\nPlease enter their hobby"
+  hobby = STDIN.gets.chomp
+  if hobby == ""
+    return "not entered"
+  end
+  hobby
+end
+
+def add_occupation
+  puts "\nPlease enter their current occupation"
+  occupation = STDIN.gets.chomp
+  if occupation == ""
+    return occupation = "not entered"
+  end
+  occupation
+end
+
+def add_nationality
+  puts "\nPlease enter their nationality"
+  nationality = STDIN.gets.chomp
+  if nationality == ""
+    return "not entered"
+  end
+  nationality
+end
+
+def add_cohort
+  puts "\nPlease enter their cohort month"
+  cohort = STDIN.gets.chomp
+  while cohort.empty?
+    puts "You didn't include their cohort - Please enter one now."
+    cohort = STDIN.gets.chomp
+    if !cohort.empty?
+      break
+    end
+  end
+  cohort
 end
 
 #call interactive menu method to run the program
